31d1aee7bdb2f15eecf6f2db2ebad2068758f572
Xeei <109275765+Xeei@users.noreply.github.com>
2025-08-07 12:09:03 +0700

    make actionfor make image docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   const [todos, setTodos] = useState([])
   const [todoInput, setTodoInput] = useState('')
-
+  const BACKEND_URL = 'http://localhost:15000/tasks'
 
   const handleChangeInput = (e) => {
     setTodoInput(e.target.value)
@@ -14,13 +14,29 @@ function App() {
   const fetchTodo = async() => {
     try {
 
-      fetch('http://localhost:15000/tasks').then((data) => {
+      fetch(BACKEND_URL).then((data) => {
         setTodos(data.json())        
       })
 
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const addTodo = async(e) => {
+
+    console.log(e)
+    // try {
+    //   fetch(BACKEND_URL, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json' // Indicate that you are sending JSON data
+    //     },
+    //     body: {}
+    //   })
+    // } catch (error) {
+    //   alert(error.message)
+    // }
   }
 
   useEffect(() => {
@@ -32,10 +48,12 @@ function App() {
         Todo App
       </h1>
       <div style={{ display: 'flex'}}>
-        <input placeholder='Name of Task' value={todoInput} onChange={handleChangeInput}/>
-        <button>
-          Submit
-        </button>
+        <form onSubmit={addTodo}>
+          <input placeholder='Name of Task' value={todoInput} onChange={handleChangeInput}/>
+          <button type='submit'>
+            Submit
+          </button>          
+        </form>
       </div>
       {todos.length > 0 && (
         <ul>
